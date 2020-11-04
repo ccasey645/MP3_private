250e003d39c86f045e9c03fa42621b7bc7d060e3
ccasey645 <O!4qR#d6y2YkRtfJB3>
2020-11-04 09:44:32 -0500

    ??

--- plsa.py
+++ plsa.py
@@ -197,8 +197,8 @@ class Corpus(object):
                     self.topic_prob[doc_index][topic_index][word_index] = self.document_topic_prob[doc_index][topic_index] * self.topic_word_prob[topic_index][word_index]
                     word_sum += self.topic_prob[doc_index][topic_index][word_index]
                 #self.topic_prob = three_d_normalize(self.topic_prob)
-                denominator = self.topic_prob[doc_index].sum(axis=0)
-                self.topic_prob[doc_index][topic_index] /= denominator
+            denominator = self.topic_prob[doc_index].sum(axis=0)
+            self.topic_prob[doc_index] /= denominator
 
 
 
