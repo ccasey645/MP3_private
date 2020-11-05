267a6ca04d33898c7360d1de1534c80674d0fcb9
ccasey645 <O!4qR#d6y2YkRtfJB3>
2020-11-05 13:21:05 -0500

    ?

--- plsa.py
+++ plsa.py
@@ -174,27 +174,12 @@ class Corpus(object):
         """
         print("E step:")
         for doc_index in range(self.number_of_documents):
-            topic_total = 0
             for topic_index in range(number_of_topics):
-                word_total = 0
                 for word_index in range(self.vocabulary_size):
                     self.topic_prob[doc_index][topic_index][word_index] = self.document_topic_prob[doc_index][topic_index] * self.topic_word_prob[topic_index][word_index]
-                    word_total += self.topic_prob[doc_index][topic_index][word_index]
 
-                    #topic_total += self.topic_prob[doc_index][topic_index][word_index]
-                #self.topic_prob[doc_index][topic_index] /= topic_total
-                # for topic_index in range(2):
-                #     self.topic_prob[doc_index][topic_index][word_index] /= total
-            self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index], is_col=True)
+                self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index], is_col=True)
             self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index])
-        # for doc_index in range(self.number_of_documents):
-        #     self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index])
-
-            #     self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index].transpose()).transpose()
-            # self.topic_prob = normalize_three_d(self.topic_prob)
-
-        # for doc_index in range(self.number_of_documents):
-        #     self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index])
 
     def maximization_step(self, number_of_topics):
         """ The M-step updates P(w | z)
@@ -206,12 +191,9 @@ class Corpus(object):
             for word_index in range(self.vocabulary_size):
                 count = 0
                 for doc_index in range(self.number_of_documents):
-                    #self.topic_word_prob[topic_index][word_index] = self.term_doc_matrix[doc_index][word_index] * self.topic_prob[doc_index][topic_index][word_index]
                     count += self.term_doc_matrix[doc_index][word_index] * self.topic_prob[doc_index][topic_index][word_index]
                 word_counts.append(count)
-            #self.topic_word_prob = normalize(self.topic_word_prob, is_col=True)
             self.topic_word_prob[topic_index, :] = normalize(np.array(word_counts).reshape(1,-1))
-        #self.topic_word_prob = normalize(self.topic_word_prob)
 
         # update P(z | d)
         for doc_index in range(self.number_of_documents):
@@ -222,13 +204,6 @@ class Corpus(object):
                     count += self.term_doc_matrix[doc_index][word_index] * self.topic_prob[doc_index][topic_index][word_index]
                 topic_counts.append(count)
             self.document_topic_prob[doc_index, :] = normalize(np.array(topic_counts).reshape(1, -1))
-        # self.document_topic_prob = normalize(self.document_topic_prob)
-        # for doc_index in range(self.number_of_documents):
-        #     for topic_index in range(number_of_topics):
-        #         for word_index in range(self.vocabulary_size):
-        #             self.document_topic_prob[doc_index][topic_index] = self.term_doc_matrix[doc_index][word_index] * self.topic_prob[doc_index][topic_index][word_index]
-        #     self.document_topic_prob = normalize(self.document_topic_prob)
-
 
     def calculate_likelihood(self, number_of_topics):
         """ Calculate the current log-likelihood of the model using
