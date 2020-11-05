8cbb6a6dd91719d938e394f39d05bf2aeb331cfe
ccasey645 <O!4qR#d6y2YkRtfJB3>
2020-11-05 10:07:48 -0500

    ???????????????????????????

--- plsa.py
+++ plsa.py
@@ -191,8 +191,9 @@ class Corpus(object):
                 #self.topic_prob[doc_index][topic_index] /= topic_total
                 # for topic_index in range(2):
                 #     self.topic_prob[doc_index][topic_index][word_index] /= total
-            self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index], is_col=True)
+                self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index], is_col=True)
             self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index])
+
             #     self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index].transpose()).transpose()
             # self.topic_prob = normalize_three_d(self.topic_prob)
 
