2e5eb60811738c5bd129e8a198d28e42a263e8e9
ccasey645 <O!4qR#d6y2YkRtfJB3>
2020-11-05 08:41:29 -0500

    ???????????????????????????

--- plsa.py
+++ plsa.py
@@ -181,12 +181,13 @@ class Corpus(object):
         print("E step:")
         for doc_index in range(self.number_of_documents):
             topic_total = 0
-            for topic_index in range(number_of_topics):
-                word_total = 0
-                for word_index in range(self.vocabulary_size):
+            for word_index in range(self.vocabulary_size):
+                for topic_index in range(number_of_topics):
+                    word_total = 0
+
                     self.topic_prob[doc_index][topic_index][word_index] = self.document_topic_prob[doc_index][topic_index] * self.topic_word_prob[topic_index][word_index]
                     word_total += self.topic_prob[doc_index][topic_index][word_index]
-                self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index], is_col=True)
+                self.topic_prob[doc_index] = normalize(self.topic_prob[doc_index])
                     #topic_total += self.topic_prob[doc_index][topic_index][word_index]
                 #self.topic_prob[doc_index][topic_index] /= topic_total
                 # for topic_index in range(2):
